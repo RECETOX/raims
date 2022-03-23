f1ffc26498dfec005eb2d41ff3b976cc344b7320
Jiri Novotny <xtracko@users.noreply.github.com>
2022-03-23 11:50:56 +0100

    Updated metacentrum script

--- run_metacentrum.sh
+++ run_metacentrum.sh
@@ -4,18 +4,19 @@
 #PBS -l walltime=8:00:00
 #PBS -m ae
 
-echo ${PBS_O_LOGNAME:?This script must be run under PBS scheduling system, execute: qsub $0}
+echo "${PBS_O_LOGNAME:?This script must be run under PBS scheduling system, execute: qsub $0}"
 
-HOSTNAME=`hostname -f`
+HOSTNAME=$(hostname -f)
 PORT=8888
 TOKEN="d08d8f7784636a70572c44c4d4e4a7131d0de87fc54f2eac"
+SINGULARITY_IMAGE="$PBS_O_WORKDIR/raims_jupyter.sif"
 
-while [[ -n "$(netstat -taln | grep $PORT)" ]]
+while netstat -taln | grep -q $PORT
 do
-	PORT=$PORT + 1
+	PORT=$((PORT + 1))
 done
 
-mail -s "Jupter session started" $PBS_O_LOGNAME << EOFmail
+mail -s "Jupyter session started" "$PBS_O_LOGNAME" << EOFmail
 Jupyter session started http://$HOSTNAME:$PORT?token=$TOKEN
 EOFmail
 
@@ -23,13 +24,16 @@ export SINGULARITY_CACHEDIR=$PBS_O_HOME
 export SINGULARITY_LOCALCACHEDIR=$SCRATCHDIR
 export SINGULARITY_TMPDIR=$SCRATCHDIR
 
-cd $PBS_O_HOME
+cd "$PBS_O_HOME" || exit $?
 
 singularity exec --nv \
-	--home $PBS_O_HOME \
-	--bind $SCRATCHDIR \
+	--home "$PBS_O_HOME" \
+	--bind "$SCRATCHDIR" \
 	--bind /storage \
-	$PBS_O_WORKDIR/msai.sif \
-	jupyter-lab --port=$PORT --NotebookApp.token=$TOKEN --notebook-dir=$PBS_O_WORKDIR
+	"$SINGULARITY_IMAGE" \
+	jupyter-lab \
+	  --port=$PORT \
+	  --NotebookApp.token=$TOKEN \
+	  --notebook-dir="$PBS_O_WORKDIR"
 
 clean_scratch
