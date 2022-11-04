c29c8e10dbd8030e322419647ffaa26f000f488f
Ales Krenek <ljocha@ics.muni.cz>
2022-11-04 13:39:44 +0100

    image; gpus

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 echo IMAGE=${IMAGE:=ljocha/raims}
+#echo IMAGE=${IMAGE:=xtracko/raims_jupyter}
+
 echo PORT=${PORT:=8888}
 echo DATADIR=${DATADIR:=$PWD}
 
-echo NVIDIA_FLAGS=${NVIDIA_FLAGS:=--gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864}
+echo GPUS=${GPUS:='"device=1"'}
+echo NVIDIA_FLAGS=${NVIDIA_FLAGS:=--gpus $GPUS --ipc=host --ulimit memlock=-1 --ulimit stack=67108864}
 
 docker run $NVIDIA_FLAGS \
 	-u $(id -u) \
 	-e HOME=/home/jovyan -v $HOME:/home/jovyan \
 	-p $PORT:$PORT \
+	-v $DATADIR/notebooks:/workspace -v $DATADIR/src/raims:/workspace/raims \
 	-v $DATADIR/data:/workspace/data -v $DATADIR/model:/workspace/model -v $DATADIR/wandb:/workspace/wandb\
-	-ti \
+	-ti --rm \
 	$IMAGE jupyter-lab
 
