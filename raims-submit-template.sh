b8e57eebdfa26fd83c8d67d66655513ceab4c5de
Ales Krenek <ljocha@ics.muni.cz>
2022-11-22 16:03:03 +0100

    submit

---
+++ raims-submit-template.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+base=/storage/brno2/home/ljocha/raims/
+# gpus=1
+cluster=XXX
+
+for heads in 6 12 ; do
+	for layers in 6 12 ; do
+		for embed in 120 300 900; do
+			for batch in 64 128 256; do
+				for half in "" --half; do
+					for gpus in 1 2 4; do
+			
+ 
+
+#heads=6
+#layers=12
+#embed=300
+#batch=128
+#half=--half
+
+fullname=raims_${cluster}_H${heads}_L${layers}_E${embed}_G${gpus}_B${batch}_${half}
+
+cat - >$fullname.sh <<EOF
+#!/bin/bash
+
+#PBS -N $fullname
+#PBS -q gpu
+#PBS -l select=1:ncpus=2:mem=16gb:scratch_local=10gb:ngpus=$gpus:cluster=$cluster
+#PBS -l walltime=4:00:00
+
+trap 'rm -r \$SCRATCHDIR' TERM EXIT
+
+cd $base
+tar cf - raims_job.py data model wandb/key | (cd \$SCRATCHDIR && tar xpf - )
+
+cd \$SCRATCHDIR
+
+singularity run --nv -B \$SCRATCHDIR:/work --pwd /work $base/ljocha-raims.sif \
+	python3 raims_job.py --heads $heads --layers $layers --embed $embed $half --gpus $gpus \
+		--batch $batch --name $cluster 
+	
+EOF
+
+done;	done;	done;	done;	done;	done
+
+qsub $fullname.sh
